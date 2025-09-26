375e2ce002269616863fd26f3d175bf07d2a398f
NoPurposeInLife <30126475+NoPurposeInLife@users.noreply.github.com>
2025-09-27 03:36:11 +0800

    A
    A

--- unrestricted_file_upload_poc.c
+++ unrestricted_file_upload_poc.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-
-int main() {
-  printf("Unrestricted File Upload POC");
-  return 0;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1092e1a1506be8753dcc766dd03f8c5c1d9a802924a1c3f25390bdcdf19aa81
+size 93
